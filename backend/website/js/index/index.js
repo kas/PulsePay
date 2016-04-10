2aa4ea45668f6918b505255e789e7d071881918c
Yehya Awad <yehyaawad@hotmail.com>
2016-04-10 00:35:04 +0000

    Integrated post request

---
+++ backend/website/js/index/index.js
@@ -0,0 +1,33 @@
+/* {
+  "Transaction": {
+    "TransactionAmount": "101.00",
+    "ReferenceNumber": "1",
+    "PartialApprovedFlag": "false",
+    "CustomerID": "345",
+    "OrderSource": "ecommerce"
+  },
+  "Address": {
+    "BillingName": "John & Mary Smith",
+    "BillingCountry": "US",
+    "BillingAddress1": "1 Main St.",
+    "BillingCity": "Burlington",
+    "BillingZipcode": "01803-3747",
+    "BillingState": "MA"
+  },
+  "Reports": {
+    "ReportGroup": "1243"
+  },
+  "Application": {
+    "ApplicationID": "0314efb525d8f740f1a33bd71"
+  },
+  "Credentials": {
+    "AcceptorID": "1147003"
+  },
+  "Card": {
+    "CardNumber": "4457010000000009",
+    "Type": "VI",
+    "CVV": "222",
+    "ExpirationMonth": "01",
+    "ExpirationYear": "16"
+  }
+}*/
