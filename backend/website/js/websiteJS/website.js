4e4ff561129dea4ecd868778d8d6afd82eb0408d
Yehya Awad <yehyaawad@hotmail.com>
2016-04-10 08:03:56 +0000

    Implementing payment

---
+++ backend/website/js/websiteJS/website.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+    /* The products stored in an array */
+   var products = [
+       {
+           image: "img/index/rs.jpg",
+           title: "Under Armour Grey Hoodie",
+           price: "99.99"
+       },
+       {
+           image: "img/index/coffee.png",
+           title: "Peet's Reserve Coffee",
+           price: "499.99"   
+       },
+       {
+           image: "img/index/ATH.jpg",
+           title: "Audio-Technica ATH-M50X",
+           price: "129.99"   
+       },
+       {
+           image: "img/index/m3Dp.jpg",
+           title: "MakerBot Replicator Mini3D",
+           price: "999.99"   
+       }
+    ];
+    
+    /* The buttons stored in an array */
+   var buttons = $("hvr-icon-float-away");
+   
+   /* The buttons click functions */
+   buttons.click(function(){
+       
+   });
+});
